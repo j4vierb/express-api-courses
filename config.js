934357abea0fd3916b7b131faeb886363a572f66
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-06-14 12:11:13 -0500

    Add dotenv as development dependency to include config.js in the repository

---
+++ config.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+const _ = dotenv.config();
+
+const config = {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: 60000
+  },
+  listPerPage: 10,
+};
+
+export default config;
